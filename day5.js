5ba3ee659fd6ddd9fb8821bf4f838418e22fa379
practicalcodewithshiva <practicalcodewithshiva@gmail.com>
2025-10-03 07:45:29 +0530

    added assignments

---
+++ day5.js
@@ -0,0 +1,59 @@
+
+// // console.log(name);
+// // const name = "Sireeesha";
+
+
+// // scopes global local 
+
+// // hoisting 
+// // global phase memory creation and memory execution
+
+
+
+
+// data store cheyali ante manaku 2 ways 
+
+// Primitives way data store cheydam single values emaina
+
+// js
+
+
+
+
+// data store 
+
+
+// int 
+// float 
+
+// type.  and value 
+// String userName= "Devgnan"
+// Number password = 2433453454
+
+// String.  ""    userName= "Devgnan"
+// Number        password = 234234234234
+// Boolean.      isLoggedin= true or false
+// undefined
+// null
+
+
+// I agree to the Terms of Service and Privacy Policy.  true or false
+
+
+
+
+// 7799033345 Number     010101010101 computer memory lo
+
+// "shivakumarvakiti"   10101010101010
+// isLoggedin.  true or false   1  0
+
+// const name = 
+
+
+// NoN primitives way single value multiple value 
+
+
+
+
+
+
